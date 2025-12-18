40be5a1d8a06580f73caf91058bd4a990256bb8d
Captain Black <shgawa.cp@gmail.com>
2025-12-18 22:46:48 +0300

    updating url connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoos = require("mongoose");
+mongoos.set('strictQuery', false);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://Nano:Tech1133@cluster0.pe6hke6.mongodb.net/?appName=Cluster0";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoos.connect(process.env.MANGO_URI);
+    const conn = await mongoos.connect(MONGODB_URI);
     console.log(`mongodb connected ${conn.connection.host}`.cyan.underline);
   } catch (err) {
     console.log(err);
